5226da6d0bd01781844380d27fc7edabff509fce
David <davidshack97@gmail.com>
2021-12-07 14:19:40 -0600

    Update Text

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,7 +3,7 @@
         <h1 class="text-center text-3xl mb-10">About Me</h1>
         <div class="leading-7 md:text-lg md:leading-10">
             <p class="my-5">I'm an enthusiastic Full-Stack Developer with an expanding interest in solving problems using efficient solutions. I learned HTML and CSS in middle school and over the years I have been building my development skills alongside my communication skills and professionalism.</p>
-            <p class="my-5">After college I had the privilege to work for a daycare management software, Fastoche, where I strengthened my PHP, MySQL, JavaScript and CSS knowledge.</p>
+            <p class="my-5">After college I had the privilege to work for a daycare management software, Fastoche, where I strengthened my PHP, MySQL, JavaScript and CSS knowledge. Being a Full-stack Developer I had the opportunity to be a part of creating amazing features for daycare centres while expanding my knowledge in both front-end and back-end development.</p>
             <p class="my-5">Currently, I am a Software Developer for Scootaround, a great MaaS (Mobility as a Service) company that offers mobility equipment rentals in over 2500 locations. Every day I apply my skills to help refine, refactor, and reform internal software to aid our day-to-day operation both frontend and backend. Scootaround has taught me how to effectively work and communicate within a team and stretch my critical thinking around business logic.</p>
             <p class="my-5">Throughout my developer journey I have learned a few things about being a developer:
             <ul class="text-sm md:text-base text-sky-900 font-semibold sm:pl-10">
